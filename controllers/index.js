d773f0a0e2f73cd191b6e788435bc96418ee5f76
kevinccbsg <kevin2906@hotmail.com>
2016-09-27 14:38:50 +0200

    controller and model directory added

---
+++ controllers/index.js
@@ -0,0 +1,22 @@
+'use strict'
+const Beer = require('../models/beer');
+
+exports.getBeers = function(req, res, next) {
+
+};
+
+exports.postBeers = function(req, res, next) {
+
+};
+
+exports.getBeer = function(req, res, next) {
+
+};
+
+exports.putBeer = function(req, res, next) {
+
+};
+
+exports.deleteBeers = function(req, res, next) {
+
+};
