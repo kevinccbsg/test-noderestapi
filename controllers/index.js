daac1f77fc990f28dc9ba7c2a58e9c6c2302788b
kevinccbsg <kevin2906@hotmail.com>
2016-09-27 16:46:51 +0200

    Controller of beers

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,66 @@
 'use strict'
 const Beer = require('../models/beer');
+const helper = require('../config/helperFunctions');
 
-exports.getBeers = function(req, res, next) {
+module.exports.postBeers = (req, res, next) => {
+	// Instance of Beer model
+	let beer = new Beer();
 
+	beer.name = req.body.name;
+	beer.type = req.body.name;
+	beer.quantity = req.body.quantity;
+
+	beer.save((err) => {
+		if (err) {
+			return helper.error(res, next, 'Error saving user to database: '+ err, 500);
+		}
+		return helper.success(res, next, beer, 201);
+	});
 };
 
-exports.postBeers = function(req, res, next) {
-
+module.exports.getBeers = (req, res, next) => {
+	// Use the beer model to find all
+	Beer.find((err, beers) => {
+		if (err) {
+			return helper.error(res, next, 'Error to get beers of database: '+ err, 404);
+		}
+		return helper.success(res, next, beers, 200);
+	});
 };
 
-exports.getBeer = function(req, res, next) {
+module.exports.getBeer = (req, res, next) => {
+	let beerId = req.params.beer_id;
+	Beers.findById(beerId, (err, beer) => {
+		if (err) {
+			return helper.error(res, next, 'Error to get beer of database: '+ err, 404);
+		}
+		return helper.success(res, next, beer, 200);
+	});
+};
+	
+module.exports.putBeer = (req, res, next) => {
+	let beerId = req.params.beer_id;
+	Beer.findById(beerId, (err, beer) => {
+		if (err) {
+			return helper.error(res, next, 'Error to get beer of database: '+ err, 404);
+		}
+		beer.name = req.body.name;
+		beer.type = req.body.name;
+		beer.quantity = req.body.quantity;
 
+		beer.save((err) => {
+			if (err) return helper.error(res, next, 'Error updating user to database: '+ err, 500);
+
+			return helper.success(res, next, beer, 202);
+		});
+	});
 };
 
-exports.putBeer = function(req, res, next) {
+module.exports.deleteBeers = (req, res, next) => {
+	let beerId = req.params.beer_id;
+	Beer.findByIdAndRemove(beerId, (err) => {
+		if (err) return helper.error(res, next, 'Error to delete beer of database: '+ err, 404);
 
-};
-
-exports.deleteBeers = function(req, res, next) {
-
+		return helper.success(res, next, 'Beer removed from the locker', 202);
+	});
 };
