92f8f117acba8bf76cb0ac78c0d5448fc992c691
kevinccbsg <kevin2906@hotmail.com>
2016-09-28 00:43:12 +0200

    authcontroller created

---
+++ controllers/authController.js
@@ -0,0 +1,20 @@
+'use strict'
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+const User = require('../models/user');
+
+passport.use(new BasicStrategy(
+	function(userid, password, callback) {
+		User.findOne(, (err, user) => {
+			if (err) return callback(err);
+			if (!user) return callback(null, false);
+			user.verifyPassword(password, function(err, isMatch){
+				if (err) return callback(err);
+				if (!isMatch) return callback(null, false);
+				return callback(null, user);
+			});
+		});
+	}
+));
+
+exports.isAthenticated = passport.authenticate('basic', { session:false });
