8b0fca5c04bd4afb47ff9a9e154d1130b8a9e3f4
kevinccbsg <kevin2906@hotmail.com>
2016-09-27 14:37:57 +0200

    Add router tu app of express

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const route = require('./router');
 let port = process.env.PORT || 3000;
 // Use the body-parser package in our app
 app.use(bodyParser.json());
@@ -10,9 +11,7 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-app.get('/', (req, res, next) => {
-	res.send('hello world');
-});
+app.use('/api/1.0', router);
 
 app.listen(port, () => {
 	console.log(`Listen on Port ${port}`);
