3ee82e3c27a26cf0bfd787b7d4fd5a7501a6cae3
kevin Martínez <kevinccbsg@gmail.com>
2020-06-13 22:34:36 +0200

    fix this old api payload

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,9 @@ module.exports.postUser = (req, res, next) => {
 	user.save((err) => {
 		if (err) return helper.error(res, next, 'Error saving user to database: '+ err, 500);
 
-		return helper.success(res, next, user, 201);
+		return helper.success(res, next, {
+      username: req.body.username,
+    }, 201);
 	});
 };
 
@@ -21,7 +23,9 @@ module.exports.postUser = (req, res, next) => {
 module.exports.getUsers = (req, res, next) => {
 	User.find((err, users) => {
 		if (err) return helper.error(res, next, 'Error to get users of database: '+ err, 404);
-
+    const returnUsers = users.map(user => ({
+      username: user.username,
+    }));
 		return helper.success(res, next, users, 200);
 	});
 };
