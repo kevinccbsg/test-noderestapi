5ec0b4acb60941bf9c3f05cec499b52fb756a959
kevin Martínez <kevinccbsg@gmail.com>
2020-06-13 22:25:45 +0200

    feat: add swagger docs

--- router/index.js
+++ router/index.js
@@ -5,17 +5,71 @@ const beerController = require('../controllers/beerController');
 const userController = require('../controllers/userController');
 
 router.route('/beers')
+  /**
+   * GET /beers
+   * @summary Beer list
+   * @tags Beers - Everything about Beers
+   * @return {array<object>} 200 - Beer list
+   * @return {string} default - Error request
+   */
 	.get(beerController.getBeers)
+  /**
+   * POST /beers
+   * @summary Create beer endpoint
+   * @tags Beers - Everything about Beers
+   * @param {object} request.body.required - Create beer
+   * @return {object} 201 - Beer created
+   * @return {string} default - Error request
+   */
 	.post(beerController.postBeers);
 
-//
+
 router.route('/beers/:beer_id')
+  /**
+   * GET /beers/{beer_id}
+   * @summary Beer Item
+   * @tags Beers - Everything about Beers
+   * @return {object} 200 - Beer item
+   * @return {string} 404 - Error to get beer of database
+   * @return {string} default - Error request
+   */
 	.get(beerController.getBeer)
+  /**
+   * PUT /beers/{beer_id}
+   * @summary Beer Item
+   * @tags Beers - Everything about Beers
+   * @return {object} 200 - Beer item
+   * @return {string} 404 - Error to get beer of database
+   * @return {string} default - Error request
+   */
 	.put(beerController.putBeer)
+  /**
+   * DELETE /beers/{beer_id}
+   * @summary Delete beer
+   * @tags Beers - Everything about Beers
+   * @return {string} 202 - Beer removed from the locker
+   * @return {string} 404 - Error to get beer of database
+   * @return {string} default - Error request
+   */
 	.delete(beerController.deleteBeer);
 
 router.route('/users')
+  /**
+   * GET /users
+   * @summary Beer list
+   * @tags Users - Everything about Users
+   * @return {array<object>} 200 - Beer list
+   * @return {string} default - Error request
+   */
 	.get(userController.getUsers)
+  /**
+   * POST /users
+   * @summary Create beer endpoint
+   * @tags Users - Everything about Users
+   * @param {object} request.body.required - Create user
+   * @return {object} 201 - User created
+   * @return {string} default - Error request
+   */
 	.post(userController.postUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,25 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+
 const router = require('./router');
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+const swaggerOptions = {
+  info: {
+    description: 'Documentation for Node REST API',
+    title: 'NodeRESTAPI',
+    version: '1.0.0',
+    contact: {
+      name: 'Kevin Martínez',
+      email: 'kevinccbsg@gmail.com',
+    },
+  },
+  servers: [],
+  baseDir: __dirname,
+  filesPattern: './router/**.js',
+};
 
 mongoose.connect('mongodb://localhost:27017/beerlocker', (err) => {
 	if (err) { throw err; }
@@ -14,6 +31,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
+expressJSDocSwagger(app)(swaggerOptions);
 
 app.use('/api/1.0', router);
 
